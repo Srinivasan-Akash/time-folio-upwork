a9efd908250e2666cdd19c2861d928154639b172
Srinivasan-Akash <prashanthiakash@gmail.com>
2024-05-22 22:24:43 +0530

    Added preloader

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,36 +1,114 @@
+import { useEffect, useState } from "react";
 import "./dashboard.scss";
+import { account } from "../../appwrite/appwrite.config";
+import { useNavigate } from "react-router-dom";
+import Loader from "../Loader/Loader";
 
 export default function Dashboard() {
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function getAuthStatus() {
+      try {
+        const user = await account.get();
+        console.log(user);
+        setLoading(true);
+
+        if (Object.keys(user).length === 0) {
+          navigate("/login");
+        }
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setLoading(false); // Set loading to false after fetching user
+      }
+    }
+
+    getAuthStatus();
+  });
+
   return (
-    <div className="dashboard">
-      <div className="sidebar">
-        <div className="planets">
-          <div className="options">
-            <h2>Create Planets</h2>
-            <div>
+    <>
+      {loading === true ? (
+        <Loader></Loader>
+      ) : (
+        <div className="dashboard">
+          <div className="sidebar">
+            <div className="planets">
+              <div className="options">
+                <h2>Create Planets</h2>
+                <div>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    className="lucide lucide-circle-plus"
+                  >
+                    <circle cx="12" cy="12" r="10" />
+                    <path d="M8 12h8" />
+                    <path d="M12 8v8" />
+                  </svg>
+                </div>
+              </div>
+
+              {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((el) => {
+                return (
+                  <div className="planet">
+                    <h3>Digital Ocean Team</h3>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      className="lucide lucide-trash-2"
+                    >
+                      <path d="M3 6h18" />
+                      <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
+                      <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
+                      <line x1="10" x2="10" y1="11" y2="17" />
+                      <line x1="14" x2="14" y1="11" y2="17" />
+                    </svg>
+                  </div>
+                );
+              })}
+            </div>
+
+            <button>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
+                width="30"
+                height="30"
                 viewBox="0 0 24 24"
                 fill="none"
                 stroke="currentColor"
                 stroke-width="2"
                 stroke-linecap="round"
                 stroke-linejoin="round"
-                class="lucide lucide-circle-plus"
+                className="lucide lucide-crown"
               >
-                <circle cx="12" cy="12" r="10" />
-                <path d="M8 12h8" />
-                <path d="M12 8v8" />
+                <path d="M11.562 3.266a.5.5 0 0 1 .876 0L15.39 8.87a1 1 0 0 0 1.516.294L21.183 5.5a.5.5 0 0 1 .798.519l-2.834 10.246a1 1 0 0 1-.956.734H5.81a1 1 0 0 1-.957-.734L2.02 6.02a.5.5 0 0 1 .798-.519l4.276 3.664a1 1 0 0 0 1.516-.294z" />
+                <path d="M5 21h14" />
               </svg>
-            </div>
+              UPGRADE TO PRO
+            </button>
           </div>
-         
-          {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((el) => {
-            return (
-              <div className="planet">
-                <h3>Digital Ocean Team</h3>
+
+          <div className="main">
+            <nav>
+              <h2>Digital Ocean Team</h2>
+              <button>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="24"
@@ -41,70 +119,25 @@ export default function Dashboard() {
                   stroke-width="2"
                   stroke-linecap="round"
                   stroke-linejoin="round"
-                  className="lucide lucide-trash-2"
+                  className="lucide lucide-map-pinned"
                 >
-                  <path d="M3 6h18" />
-                  <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
-                  <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
-                  <line x1="10" x2="10" y1="11" y2="17" />
-                  <line x1="14" x2="14" y1="11" y2="17" />
+                  <path d="M18 8c0 4.5-6 9-6 9s-6-4.5-6-9a6 6 0 0 1 12 0" />
+                  <circle cx="12" cy="8" r="2" />
+                  <path d="M8.835 14H5a1 1 0 0 0-.9.7l-2 6c-.1.1-.1.2-.1.3 0 .6.4 1 1 1h18c.6 0 1-.4 1-1 0-.1 0-.2-.1-.3l-2-6a1 1 0 0 0-.9-.7h-3.835" />
                 </svg>
-              </div>
-            );
-          })}
+                <h2>Add New Location</h2>
+              </button>
+            </nav>
+            <section>
+              <div className="card"></div>
+              <div className="card"></div>
+              <div className="card"></div>
+              <div className="card"></div>
+              <div className="card"></div>
+            </section>
+          </div>
         </div>
-
-        <button>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="30"
-            height="30"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="lucide lucide-crown"
-          >
-            <path d="M11.562 3.266a.5.5 0 0 1 .876 0L15.39 8.87a1 1 0 0 0 1.516.294L21.183 5.5a.5.5 0 0 1 .798.519l-2.834 10.246a1 1 0 0 1-.956.734H5.81a1 1 0 0 1-.957-.734L2.02 6.02a.5.5 0 0 1 .798-.519l4.276 3.664a1 1 0 0 0 1.516-.294z" />
-            <path d="M5 21h14" />
-          </svg>
-          UPGRADE TO PRO
-        </button>
-      </div>
-
-      <div className="main">
-        <nav>
-          <h2>Digital Ocean Team</h2>
-          <button>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              className="lucide lucide-map-pinned"
-            >
-              <path d="M18 8c0 4.5-6 9-6 9s-6-4.5-6-9a6 6 0 0 1 12 0" />
-              <circle cx="12" cy="8" r="2" />
-              <path d="M8.835 14H5a1 1 0 0 0-.9.7l-2 6c-.1.1-.1.2-.1.3 0 .6.4 1 1 1h18c.6 0 1-.4 1-1 0-.1 0-.2-.1-.3l-2-6a1 1 0 0 0-.9-.7h-3.835" />
-            </svg>
-            <h2>Add New Location</h2>
-          </button>
-        </nav>
-        <section>
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
-        </section>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
---
+++ src/pages/Loader/Loader.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import loader from "./loader.json"
+import Lottie from 'lottie-react'
+import "./loader.scss"
+
+export default function Loader() {
+  return (
+    <div className='loader'>
+        <Lottie animationData={loader}>
+
+        </Lottie>
+    </div>
+  )
+}
