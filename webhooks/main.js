3322147b7717630580c02b29b2dc697b3cdc7ebb
Srinivasan-Akash <prashanthiakash@gmail.com>
2024-05-25 15:43:28 +0530

    .

--- webhooks/main.js
+++ webhooks/main.js
@@ -1,34 +1,22 @@
-import { Client, Users } from 'node-appwrite';
-import crypto from 'crypto';
+const { Client, Users } = require('node-appwrite');
+const crypto = require('crypto');
 
-export default async (context) => {
+module.exports = async (context) => {
   const { req, res, log, error } = context;
 
-  const requiredEnvVars = [
-    'https://cloud.appwrite.io/v1',
-    '664c966b003c00aba212',
-    'ee704fcf107eb14f38ecb7972401d3b6251ca8b68d50ce1badbb087ef44e6209002fe4153090b1fddc2fd1251425ccf68a89e29dde9e61a17e9aede37b24fe813fbbdf8d873e84d01e716ff93321e59bcf22ee3031c0dda37f395bfd67e3f2df448b903985e8fd825dcea848179e353acea2887afce346fc14ee5a6a4f536771',
-    'time-folio'
-  ];
-
-  for (const varName of requiredEnvVars) {
-    if (!process.env[varName]) {
-      throw new Error(`Environment variable ${varName} is missing`);
-    }
-  }
 
   // Initialize Appwrite client
   const client = new Client()
-    .setEndpoint(process.env.APPWRITE_ENDPOINT)
-    .setProject(process.env.APPWRITE_PROJECT_ID)
-    .setKey(process.env.APPWRITE_API_KEY);
+    .setEndpoint("https://cloud.appwrite.io/v1")
+    .setProject("664c966b003c00aba212")
+    .setKey("ee704fcf107eb14f38ecb7972401d3b6251ca8b68d50ce1badbb087ef44e6209002fe4153090b1fddc2fd1251425ccf68a89e29dde9e61a17e9aede37b24fe813fbbdf8d873e84d01e716ff93321e59bcf22ee3031c0dda37f395bfd67e3f2df448b903985e8fd825dcea848179e353acea2887afce346fc14ee5a6a4f536771");
 
   const users = new Users(client);
 
   // Validate webhook signature
   const validateWebhook = (context) => {
     try {
-      const secret = process.env.LEMON_SQUEEZY_WEBHOOK_SECRET;
+      const secret = 'time-folio';
       const hmac = crypto.createHmac('sha256', secret);
       const digest = Buffer.from(
         hmac.update(context.req.bodyRaw).digest('hex'),
@@ -81,6 +69,6 @@ export default async (context) => {
       return res.json({ success: false }, 500);
     }
   } else {
-    return res.send('Not Found', 404);
+    return res.send('HELLO !!', 200);
   }
 };
